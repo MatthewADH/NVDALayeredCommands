e9629ab7b3955924b2a1a0359f35b07d41d3015a
MatthewADH <matthew.duffell.h@gmail.com>
2026-04-02 20:52:20 -0700

    build variables

--- buildVars.py
+++ buildVars.py
@@ -26,22 +26,22 @@ Press NVDA+Alt+Space to activate layered command mode
 Press slash or question mark in any layer to get a list of commands for that layer
 Activation gesture can be modified in input gestures -> input"""),
 	# version
-	addon_version="0.3",
+	addon_version="1.0",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_(""" """),
+	addon_changelog=_("""Initial Release"""),
 	# Author(s)
 	addon_author="Matthew Duffell-Hoffman   matthew.duffell.h@gmail.com",
 	# URL for the add-on documentation support
 	addon_url=None,
 	# URL for the add-on repository where the source code can be found
-	addon_sourceURL=None,
+	addon_sourceURL="https://github.com/MatthewADH/NVDALayeredCommands",
 	# Documentation file name
 	addon_docFileName="readme.html",
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
-	addon_minimumNVDAVersion="2025.3.0",
+	addon_minimumNVDAVersion="2025.1",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	addon_lastTestedNVDAVersion="2025.3.2",
+	addon_lastTestedNVDAVersion="2025.3.3",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
